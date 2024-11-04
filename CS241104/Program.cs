9cf24f715a5878273585cdb66b17e4390606f612
Gyurkovics Dominik <65687471+gydoma@users.noreply.github.com>
2024-11-04 10:19:47 +0100

    Add project files.

---
+++ CS241104/Program.cs
@@ -0,0 +1,95 @@
+﻿using CS241104;
+using System.Text;
+
+List<Versenyzok> versenyzok = [];
+using StreamReader sr = new("..\\..\\..\\res\\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream)
+{
+    versenyzok.Add(new(sr.ReadLine()));
+}
+Console.WriteLine($"Versenyzők száma: {versenyzok.Count}");
+
+var f11 = versenyzok.Count(v => v.Kategoria == "elit");
+Console.WriteLine($"Elit kategóriában: {f11} versenyző");
+var f12 = versenyzok.Count(v => v.Kategoria == "elit junior");
+Console.WriteLine($"Elit Juniro Kategóriában: {f12} versenyzo");
+var f13 = versenyzok.Count(v => v.Kategoria == "25-29");
+Console.WriteLine($"25-29 Kategóriában: {f13} versenyző");
+Console.WriteLine("");
+
+var f21 = versenyzok.Where(v => !v.Nem).Average(v => DateTime.Now.Year - v.Szul);
+Console.WriteLine($"Női versenyzők átlagéletkora: {f21:0.00}");
+var f22 = versenyzok.Where(v => v.Nem).Average(v => DateTime.Now.Year - v.Szul);
+Console.WriteLine($"Férfi versenyzők átlagéletkora: {f22:0.00}");
+var f23 = versenyzok.Average(v => DateTime.Now.Year - v.Szul);
+Console.WriteLine("");
+
+var f31 = versenyzok.Sum(v => v.Versenyidok["kerekpar"].TotalHours);
+Console.WriteLine($"A versenyzők kerékpározással töltött összideje: {f31:0.00} óra");
+var f32 = versenyzok.Sum(v => v.Versenyidok["futas"].TotalHours);
+Console.WriteLine($"A versenyzők futással töltött összideje: {f32:0.00} óra");
+var f33 = versenyzok.Sum(v => v.Versenyidok["uszas"].TotalHours);
+Console.WriteLine($"A versenyzők úszással töltött összideje: {f33:0.00} óra");
+Console.WriteLine("");
+
+var f41 = versenyzok.Where(v => v.Kategoria == "elit junior").Average(v => v.Versenyidok["uszas"].TotalMinutes);
+Console.WriteLine($"Átlagos úszási idő elit junior kategóriában: {f41:0.00} perc");
+var f42 = versenyzok.Where(v => v.Kategoria == "20-24").Average(v => v.Versenyidok["uszas"].TotalMinutes);
+Console.WriteLine($"Átlagos úszási idő 20-24es kategóriában: {f42:0.00} perc");
+var f43 = versenyzok.Where(v => v.Kategoria == "elit").Average(v => v.Versenyidok["uszas"].TotalMinutes);
+Console.WriteLine($"Átlagos úszási idő elit kategóriában: {f43:0.00} perc");
+Console.WriteLine("");
+
+var f51 = versenyzok.Where(v => v.Nem).MinBy(v => v.OsszIdosec);
+Console.WriteLine($"Férfi győztes: {f51}");
+var f52 = versenyzok.Where(v => v.Nem).MinBy(v => v.OsszIdosec);
+Console.WriteLine($"Női győztes: {f52}");
+Console.WriteLine($"Férfi győztes: {f51}");
+Console.WriteLine("");
+
+
+var f61 = versenyzok.GroupBy(v => v.Kategoria).OrderBy(g => g.Key);
+Console.WriteLine("Versenyt befejező versenyzők száma:");
+foreach (var grp in f61)
+{
+	Console.WriteLine($"{grp.Key,11}: {grp.Count(),2} fő, avg depó: {grp.Average(v => v.Versenyidok["depo1"].TotalMinutes + v.Versenyidok["depo2"].TotalMinutes):0.000} sec");
+}
+var f62 = versenyzok.GroupBy(v => v.Nem).OrderBy(g => g.Key);
+Console.WriteLine("Versenyt befejező versenyzők száma:");
+foreach (var grp in f62)
+{
+    if(grp.Key)
+    {
+        Console.WriteLine($"Férfi: {grp.Count(),2} fő");
+    }
+    else
+    {
+        Console.WriteLine($"Nő:    {grp.Count(),2} fő");
+    }
+}
+Console.WriteLine("Versenyt befejező versenyzők száma:");
+foreach (var grp in f61)
+{
+    Console.WriteLine($"{grp.Key,11}: {grp.Count(),2} fő, avg depó: {grp.Average(v => v.Versenyidok["depo1"].TotalMinutes + v.Versenyidok["depo2"].TotalMinutes):0.000} sec");
+}
+
+
+
+
+/*
+
+------- versenyzők száma 'elit junior' kategóriában
+------- férfi versenyzők átlagéletkora
+------- a versenyzők futással töltött összideje órában. 2tj-ig kerekítve
+------- átlagos úszási idő 20-24 kategóriában
+------- női győztes (legrövidebb összidő)
+- nemenként a versenyt befejezők száma
+
+------- versenyzők száma '25-29' korkategóriában
+------- versenyzők átlagéletkora
+------- a versenyzők úszással töltött összideje órában. 2tj-ig kerekítve
+- átlagos úszási idő elit kategóriában
+- férfi győztes (legrövidebb összidő)
+- korkategóriánként a versenyt befejezők száma
+
+ */
---
+++ CS241104/Versenyzok.cs
@@ -0,0 +1,72 @@
+﻿namespace CS241104;
+internal class Versenyzok
+{
+    public string Nev { get; set; }
+    public int Szul { get; set; }
+    public string Rajtszam { get; set; }
+    public bool Nem { get; set; }
+    public string Kategoria{ get; set; }
+    public Dictionary<string,TimeSpan> Versenyidok { get; set; }
+    public override string ToString() => $"[{Rajtszam}] {Nev} ({(Nem ? "Férfi":"Nő")}, {Kategoria})";
+
+    public int OsszIdosec => (int)Versenyidok.Values.Sum(v => v.TotalSeconds);
+
+    public Versenyzok(string sor)
+    {
+        string[] adatok = sor.Split(';');
+        Nev = adatok[0];
+        Szul = int.Parse(adatok[1]);
+        Rajtszam = adatok[2];
+        Nem = adatok[3] == "f";
+        Kategoria = adatok[4];
+        Versenyidok = new()
+        {
+            { "uszas", TimeSpan.Parse(adatok[5]) },
+            { "depo1", TimeSpan.Parse(adatok[6]) },
+            { "kerekpar", TimeSpan.Parse(adatok[7]) },
+            { "depo2", TimeSpan.Parse(adatok[8]) },
+            { "futas", TimeSpan.Parse(adatok[9]) }
+        };
+    }
+
+
+}
+/*
+ 2014-es Keszthely Triatlon verseny sprint távjának eredményeit kell
+feldolgoznod. Az forras.txt állomány sorai a versenyt befejező versenyzők adatait és az általuk elért időeredményeket tartalmazzák pontosvesszőkkel elválasztva.
+
+pl:
+Nagy Máté;1996;4;f;18-19;00:12:47;00:00:34;00:31:40;00:00:26;00:17:42
+
+Az első adat a versenyző neve
+A második adat a versenyző születésének éve.
+A harmadik szám a versenyző rajtszáma.
+A negyedik adat a neme (n = nő, f = férfi).
+Az ötödik adat a versenyző életkor szerinti kategóriáját határozza meg.
+(A kategóriák: 16-17, 18-19, 20-24, 25-29, 30-34, 35-39, 40-44,
+45-49, 50-54, elit, elit junior).
+Majd a következő öt időadat a versenyen mért időeredmények, melyek sorban a következők (óra:perc:másodperc formátumban):
+	úszás ideje,
+	első depóban töltött idő,
+	kerékpározás ideje,
+	második depóban töltött idő,
+	futás ideje.
+
+A versenyt az a versenyző nyeri, akinek a legkisebb az öt idő összege
+
+- hozz létre egy osztályt egy persenyző adatainak reprezentálására
+- to-string override, ami tartalmazza a versenyző adatait
+- olvasd be a file tartalmát egy objektumokat tartalmazó kollekcióba
+- írd ki a konzolra, hogy hány versenyző fejezte be a versenyt
+
+LINQ függvények alkalmazásával határozza határozza meg ís írja ki a konzolképernyére az alábbiakat:
+
+- versenyzők száma 'elit' kategóriában
+- női versenyzők átlagéletkora
+- a versenyzők kerékpározással töltött összideje órában. 2tj-ig kerekítve
+- átlagos úszási idő elit junior kategóriában
+- férfi győztes (legrövidebb összidő)
+- korkategóriánként a versenyt befejezők száma
+
++ korkategóriánként az átlag depóban töltött idő
+ */
